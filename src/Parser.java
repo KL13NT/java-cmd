3f0a7675617189dcfd31168641bb7cdb66792868
Nabil <nabil.tharwat@outlook.com>
2020-11-09 20:08:59 +0200

    feat: Added parsing for normal commands and |

--- src/Parser.java
+++ src/Parser.java
@@ -1,12 +1,66 @@
+import java.util.ArrayList;
+import java.util.Stack;
+
 public class Parser {
-	String[] args;// Will be filled by arguments extracted by parse method
-	String cmd;// Will be filled by the command extracted by parse method
+	public static Stack<Node> parse(String input) throws Exception {
+		Stack<Node> tree = new Stack<>();
 
-	/*** Return: true if it was able to parse user input correctly. Otherwise false* Parameter input: user command * In case of success, it should save the extracted command and arguments to args and cmd variables* It should also print error messages in case of too few arguments for a commands* eg. “cp requires 2 arguments”*/
+		String word = "";
+		ArrayList<String> current = new ArrayList<>();
 
-	// public boolean parse(String input);
+		for (int i = 0; i < input.length(); i++) {
+			char c = input.charAt(i);
 
-	// public String getCmd();
+			if (c == ' ') {
+				if (word.length() == 0)
+					continue;
 
-	// public String[] getArguments();
+				current.add(word);
+				word = "";
+
+			}
+
+			else if (c == '|') {
+				if (word.length() > 0 || current.size() > 0) {
+					if (word.length() > 0)
+						current.add(word);
+
+					tree.push(new Node("command", current));
+				}
+
+				current.clear();
+				word = "";
+			}
+
+			// else if (c == '>') {
+			// 	if (input.charAt(i - 1) == '>')
+			// 		continue; // skips second >
+
+			// 	current.add(input.substring(i, input.length() - 1));
+			// 	tree.push(new Node("redirect", current));
+
+			// 	current.clear();
+			// 	word = "";
+
+			// 	break;
+			// }
+
+			else
+				word += c;
+		}
+
+		current.add(word);
+		tree.push(new Node("command", current));
+
+		return tree;
+	}
 }
+
+
+/**
+ * Test cases
+ * command p1 p2 p3 ... [DONE]
+ * command p1 p2 p3 | command p1 p2 p3 [DONE]
+ * command p1 p2 p3 > path path path path
+ * command p1 p2 p3 >> path path path path
+ */

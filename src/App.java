6616adc328ab8e2de862bbd80bb01414eb0f7023
Mohamed Shaarawy <mhshaarawy3@outlook.com>
2020-11-11 01:18:35 +0200

    mv and cp commands finished, TODO cd

--- src/App.java
+++ src/App.java
@@ -1,4 +1,16 @@
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class App {
+
+	public static String pwd = System.getProperty("user.dir");
+
+	public static void cd(ArrayList<String> params) throws IOException
+	{
+		String Path = params.get(0);
+		//TODO: Using path objects in cd command by resolving the input path
+		//https://docs.oracle.com/javase/8/docs/api/java/nio/file/Path.html#resolve-java.lang.String-
+	}
 	public static void main(String[] args) {
 		try {
 			System.out.println("Hello");
--- src/Terminal.java
+++ src/Terminal.java
@@ -1,17 +1,78 @@
+
 /**
  * Terminal commands go here
  */
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
 
 public class Terminal {
-	// public void cp(String sourcePath, String destinationPath);
 
-	// public void mv(String sourcePath, String destinationPath);
+	public static void cp(ArrayList<String> params) throws IOException {
+		String sourcePath = params.get(0);
+		String destinationPath = params.get(1);
+		File sourceFile = new File(sourcePath);
+		File destFile = new File(destinationPath);
 
-	// public void rm(String sourcePath);
+		copy(sourceFile, destFile);
+	}
 
-	// public void pwd();
+	public void mv(ArrayList<String> params) throws IOException {
+		String sourcePath = params.get(0);
+		String destinationPath = params.get(1);
+		
+		File sourceFile = new File(sourcePath);
+		File destFile = new File(destinationPath);
 
-	// public void cat(String[] paths);
+		move(sourceFile, destFile);
+	}
+
+	private static void copy(File source, File Dest) throws IOException {
+		InputStream input = null;
+		OutputStream output = null;
+
+		try {
+			input = new FileInputStream(source);
+			output = new FileOutputStream(Dest);
+			byte[] buffer = new byte[1024];
+			int length;
+			while ((length = input.read(buffer)) > 0) {
+				output.write(buffer, 0, length);
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} finally {
+			input.close();
+			output.close();
+		}
+	}
+
+	private static void move(File source, File Dest) throws IOException {
+		InputStream input = null;
+		OutputStream output = null;
+
+		try {
+			input = new FileInputStream(source);
+			output = new FileOutputStream(Dest);
+			byte[] buffer = new byte[1024];
+			int length;
+			while ((length = input.read(buffer)) > 0) {
+				output.write(buffer, 0, length);
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} finally {
+			input.close();
+			output.close();
+			source.delete();
+		}
+	}
+
 
 	public static void main(String[] args) throws Exception {
 		System.out.println("Hello, World!");

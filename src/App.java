b1c3606ddb92d1ccaf7b0e6ed1acec2cf3eb8ccd
Mohamed Shaarawy <mhshaarawy3@outlook.com>
2020-11-11 17:53:34 +0200

    cd command ready for execution and tested: mv, cp, cd

--- src/App.java
+++ src/App.java
@@ -1,4 +1,7 @@
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 
 public class App {
@@ -7,14 +10,25 @@ public class App {
 
 	public static void cd(ArrayList<String> params) throws IOException
 	{
-		String Path = params.get(0);
 		//TODO: Using path objects in cd command by resolving the input path
 		//https://docs.oracle.com/javase/8/docs/api/java/nio/file/Path.html#resolve-java.lang.String-
+		
+		Path PWD = Paths.get(pwd);
+		Path inputPath = Paths.get(params.get(0));
+		String resolvedPath = PWD.resolve(inputPath).toFile().getCanonicalPath();
+		
+		if (Files.exists(Paths.get(resolvedPath))){
+			System.out.println("new Path: " + resolvedPath);
+			pwd = resolvedPath;	
+
+		}else System.out.println("The system cannot find the path specified.");
 	}
 	public static void main(String[] args) {
 		try {
 			System.out.println("Hello");
-
+			ArrayList<String> test = new ArrayList<String>();
+			test.add("./source");
+			cd(test);
 			// Invoking commands can be done using reflection
 
 		} catch (Exception e) {
--- src/Terminal.java
+++ src/Terminal.java
@@ -4,7 +4,6 @@
  */
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -22,7 +21,7 @@ public class Terminal {
 		copy(sourceFile, destFile);
 	}
 
-	public void mv(ArrayList<String> params) throws IOException {
+	public static void mv(ArrayList<String> params) throws IOException {
 		String sourcePath = params.get(0);
 		String destinationPath = params.get(1);
 		
@@ -32,40 +31,38 @@ public class Terminal {
 		move(sourceFile, destFile);
 	}
 
-	private static void copy(File source, File Dest) throws IOException {
+	// TEST COMPLETED
+	private static void copy(File source, File dest) throws IOException {
 		InputStream input = null;
 		OutputStream output = null;
 
 		try {
 			input = new FileInputStream(source);
-			output = new FileOutputStream(Dest);
+			output = new FileOutputStream(dest);
 			byte[] buffer = new byte[1024];
 			int length;
 			while ((length = input.read(buffer)) > 0) {
 				output.write(buffer, 0, length);
 			}
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
 		} finally {
 			input.close();
 			output.close();
 		}
 	}
 
-	private static void move(File source, File Dest) throws IOException {
+	// TEST COMPLETED
+	private static void move(File source, File dest) throws IOException {
 		InputStream input = null;
 		OutputStream output = null;
 
 		try {
 			input = new FileInputStream(source);
-			output = new FileOutputStream(Dest);
+			output = new FileOutputStream(dest);
 			byte[] buffer = new byte[1024];
 			int length;
 			while ((length = input.read(buffer)) > 0) {
 				output.write(buffer, 0, length);
 			}
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
 		} finally {
 			input.close();
 			output.close();
@@ -75,6 +72,19 @@ public class Terminal {
 
 
 	public static void main(String[] args) throws Exception {
-		System.out.println("Hello, World!");
+		try
+		{
+			System.out.println("Hello, World!");
+	
+			ArrayList<String> test = new ArrayList<String>();
+			test.add("F:\\College\\Operating Systems\\haha.txt");
+			test.add("test.txt");
+			mv(test);
+
+		}
+		catch (Exception e)
+		{
+			System.out.println(e);
+		}
 	}
 }

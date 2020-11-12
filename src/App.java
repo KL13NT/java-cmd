df26414385af03b10c4be9d901e6e0b69ed4c0be
Mohamed Shaarawy <mhshaarawy3@outlook.com>
2020-11-12 13:31:36 +0200

    cd moved to Terminal, fixed move and copy features

--- src/App.java
+++ src/App.java
@@ -8,27 +8,11 @@ public class App {
 
 	public static String pwd = System.getProperty("user.dir");
 
-	public static void cd(ArrayList<String> params) throws IOException
-	{
-		//TODO: Using path objects in cd command by resolving the input path
-		//https://docs.oracle.com/javase/8/docs/api/java/nio/file/Path.html#resolve-java.lang.String-
-		
-		Path PWD = Paths.get(pwd);
-		Path inputPath = Paths.get(params.get(0));
-		String resolvedPath = PWD.resolve(inputPath).toFile().getCanonicalPath();
-		
-		if (Files.exists(Paths.get(resolvedPath))){
-			System.out.println("new Path: " + resolvedPath);
-			pwd = resolvedPath;	
 
-		}else System.out.println("The system cannot find the path specified.");
-	}
 	public static void main(String[] args) {
 		try {
 			System.out.println("Hello");
 			ArrayList<String> test = new ArrayList<String>();
-			test.add("./source");
-			cd(test);
 			// Invoking commands can be done using reflection
 
 		} catch (Exception e) {
--- src/Terminal.java
+++ src/Terminal.java
@@ -8,6 +8,9 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 
 public class Terminal {
@@ -15,20 +18,29 @@ public class Terminal {
 	public static void cp(ArrayList<String> params) throws IOException {
 		String sourcePath = params.get(0);
 		String destinationPath = params.get(1);
-		File sourceFile = new File(sourcePath);
-		File destFile = new File(destinationPath);
 
-		copy(sourceFile, destFile);
+		if (Files.exists(Paths.get(sourcePath)))
+		{
+			File sourceFile = new File(sourcePath);
+			File destFile = new File(destinationPath);
+	
+			copy(sourceFile, destFile);
+		}
+		else System.out.println("Source file not found");
 	}
 
 	public static void mv(ArrayList<String> params) throws IOException {
 		String sourcePath = params.get(0);
 		String destinationPath = params.get(1);
 		
-		File sourceFile = new File(sourcePath);
-		File destFile = new File(destinationPath);
-
-		move(sourceFile, destFile);
+		if (Files.exists(Paths.get(sourcePath)))
+		{
+			File sourceFile = new File(sourcePath);
+			File destFile = new File(destinationPath);
+		
+			move(sourceFile, destFile);
+		}
+		else System.out.println("Source file not found");
 	}
 
 	// TEST COMPLETED
@@ -36,40 +48,50 @@ public class Terminal {
 		InputStream input = null;
 		OutputStream output = null;
 
-		try {
-			input = new FileInputStream(source);
-			output = new FileOutputStream(dest);
-			byte[] buffer = new byte[1024];
-			int length;
-			while ((length = input.read(buffer)) > 0) {
-				output.write(buffer, 0, length);
-			}
-		} finally {
-			input.close();
-			output.close();
+		input = new FileInputStream(source);
+		output = new FileOutputStream(dest);
+		byte[] buffer = new byte[1024];
+		int length;
+
+		while ((length = input.read(buffer)) > 0) {
+			output.write(buffer, 0, length);
 		}
+		input.close();
+		output.close();
 	}
 
 	// TEST COMPLETED
 	private static void move(File source, File dest) throws IOException {
-		InputStream input = null;
-		OutputStream output = null;
+        InputStream input = null;
+        OutputStream output = null;
 
-		try {
-			input = new FileInputStream(source);
-			output = new FileOutputStream(dest);
-			byte[] buffer = new byte[1024];
-			int length;
-			while ((length = input.read(buffer)) > 0) {
-				output.write(buffer, 0, length);
-			}
-		} finally {
-			input.close();
-			output.close();
-			source.delete();
-		}
+		input = new FileInputStream(source);
+        output = new FileOutputStream(dest);
+        byte[] buffer = new byte[1024];
+        int length;
+        while ((length = input.read(buffer)) > 0) {
+            output.write(buffer, 0, length);
+        }
+        input.close();
+        output.close();
+        source.delete();
 	}
 
+	public static void cd(ArrayList<String> params) throws IOException
+	{
+		//TODO: Using path objects in cd command by resolving the input path
+		//https://docs.oracle.com/javase/8/docs/api/java/nio/file/Path.html#resolve-java.lang.String-
+		
+		Path PWD = Paths.get(App.pwd);
+		Path inputPath = Paths.get(params.get(0));
+		String resolvedPath = PWD.resolve(inputPath).toFile().getCanonicalPath();
+		
+		if (Files.exists(Paths.get(resolvedPath))){
+			System.out.println("new Path: " + resolvedPath);
+			App.pwd = resolvedPath;	
+
+		}else System.out.println("The system cannot find the path specified.");
+	}
 
 	public static void main(String[] args) throws Exception {
 		try
@@ -77,8 +99,8 @@ public class Terminal {
 			System.out.println("Hello, World!");
 	
 			ArrayList<String> test = new ArrayList<String>();
-			test.add("F:\\College\\Operating Systems\\haha.txt");
 			test.add("test.txt");
+			test.add("F:\\College\\Operating Systems\\haha.txt");
 			mv(test);
 
 		}

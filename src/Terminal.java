711772091fb228aafe4751c44d27afb116e2c5ab
Nabil <nabil.tharwat@outlook.com>
2020-11-16 01:24:59 +0200

    refactor: help and args

--- src/Terminal.java
+++ src/Terminal.java
@@ -1,113 +1,58 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-/**
- * Terminal commands go here
- */
-
 public class Terminal {
-  // public void cp(String sourcePath, String destinationPath);
 
-  // public void mv(String sourcePath, String destinationPath);
+	public static String args(ArrayList<String> args) throws Exception {
+		if (args.size() < 1)
+			throw new Exception("Must provide 1 parameter");
 
-  // public void rm(String sourcePath);
+		String cmd = args.get(0);
+		String[] cmds = { "cat", "cd", "cp", "ls", "mkdir", "pwd", "rmdir" };
 
-  // public void pwd();
+		if (!Utils.arrayContains(cmds, cmd))
+			throw new Exception("Command not found");
 
-  // public void cat(String[] paths);
+		File file = new File("./args/" + cmd + ".txt");
 
-  public static void main(String[] args) throws Exception {
-    System.out.println("Hello, World!");
-    help(new ArrayList<>());
-   clear(new ArrayList<>());
-    Scanner input = new Scanner(System.in);
-    String User_input = input.nextLine();
-    ArrayList<String> params = new ArrayList<String>();
-    params.add(User_input);
-    arggs(params);
-  }
+		Scanner sc = new Scanner(file);
 
-  public static void arggs(ArrayList<String> args) throws Exception {    
-    if (args.size() < 1)
-      throw new Exception("Must provide 1 parameter");
-    switch (args.get(0)) {
-      case "cd": {
-        System.out.println("arg1: Path");
-        break;
-      }
+		String output = "";
 
-      case "cat": {
-        System.out.println("arg1: SourcePath");
-        break;
-      }
+		while (sc.hasNextLine())
+			output += sc.nextLine();
 
-      case "more": {
-        System.out.println("arg1: SourcePath");
-        break;
-      }
+		sc.close();
 
-      case "mkdir": {
-        System.out.println("arg1: SourcePath");
-        break;
-      }
+		System.out.println(output);
 
-      case "rmdir": {
-        System.out.println("arg1: SourcePath");
-        break;
-      }
+		return output;
+	}
 
-      case "mv": {
-        System.out.println("arg1: SourcePath, arg2: DestinationPath");
-        break;
-      }
+	public static String help(ArrayList<String> args) throws FileNotFoundException {
+		File file = new File("help.txt");
 
-      case "rm": {
-        System.out.println("arg1: SourcePath, arg2: DestinationPath");
-        break;
-      }
+		Scanner sc = new Scanner(file);
 
-      case "cp": {
-        System.out.println("arg1: SourcePath, arg2: DestinationPath");
-        break;
-      }
+		String output = "";
 
-      default: {
-        if (args.get(0).equalsIgnoreCase("date") || args.get(0).equalsIgnoreCase("help")
-            || args.get(0).equalsIgnoreCase("pwd") || args.get(0).equalsIgnoreCase("clear")
-            || args.get(0).equalsIgnoreCase("ls"))
-          System.out.println("no parameters needed");
-        else
-          System.out.println("this arg doesnot exist");
+		while (sc.hasNextLine())
+			output += sc.nextLine();
 
-      }
-    }
+		sc.close();
 
-  }
+		System.out.println(output);
+		return output;
+	}
 
-  public static void help(ArrayList<String> args) {
-    System.out.println("args   :    List all command arguments");
-    System.out.println("cat    :    read files from path and output it's content");
-    System.out.println("cd     :    For change the directory");
-    System.out.println("clear  :    move file to new directory");
-    System.out.println("cp     :    copy file to specific destination");
-    System.out.println("date   :    Current data/time");
-    System.out.println("exit   :    Stop all");
-    System.out.println("ls     :    show all files in specific directory");
-    System.out.println("mkdir  :    create a new directory");
-    System.out.println("more   :    Display output on screen at time");
-    System.out.println("mv     :    move file to new directory");
-    System.out.println("rm     :    remove the file(Delete)");
-    System.out.println("rmdir  :    moves the directory");
-    System.out.println("pwd    :    Display cureent directory");
+	public static String clear(ArrayList<String> args) {
+		System.out.println("\r\n".repeat(1000));
 
-  }
-
-
-
-public static void clear(ArrayList<String> args) {
-
-  System.out.println("\r\n".repeat(1000));
-}
+		return "";
+	}
 
 }
-  
--- src/Utils.java
+++ src/Utils.java
@@ -1,3 +1,12 @@
 public class Utils {
 	// All methods here should be public static. Return types are up to you.
+
+	public static Boolean arrayContains(String[] arr, String val) {
+		for (String n : arr) {
+			if (val.equalsIgnoreCase(n))
+				return true;
+		}
+
+		return false;
+	}
 }
